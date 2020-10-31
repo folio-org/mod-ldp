bd4a376f9dc3e90f54204002984164304052f0bc
Roman Ruiz-Esparza <rjruizes@ncsu.edu>
2020-10-30 21:14:11 -0400

    rm cross-origin config since okapi handles it

--- src/main/java/org/folio/ldp/QueryController.java
+++ src/main/java/org/folio/ldp/QueryController.java
@@ -25,7 +25,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
