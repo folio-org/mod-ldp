aab39483faf49ce74991e98756bb2568da2a41ee
Mike Taylor <mike@indexdata.com>
2021-05-28 14:42:09 +0100

    Tweak formatting to conform to stylecheck's preferences

--- src/main/java/org/folio/ldp/TableObjRepository.java
+++ src/main/java/org/folio/ldp/TableObjRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface TableObjRepository extends JpaRepository<TableObj, String> {
 
-  @Query("SELECT t " +
-           "FROM TableObj t " +
-           "WHERE t.tableSchema = 'local' " +
-             "OR t.tableSchema = 'folio_reporting' " +
-             "OR t.tableSchema = 'public' " +
-           "ORDER by t.tableSchema ")
+  @Query("SELECT t "
+         + "FROM TableObj t "
+         + "WHERE t.tableSchema = 'local' "
+         + "OR t.tableSchema = 'folio_reporting' "
+         + "OR t.tableSchema = 'public' "
+         + "ORDER by t.tableSchema ")
   List<TableObj> getAllTablesBySchema();
 }
