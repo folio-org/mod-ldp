09651166d551440c7bed9a167e5d2538947379ea
kurt <kurt@indexdata.com>
2021-06-28 17:50:36 -0400

    Test Implementation

--- src/test/java/org/folio/ldp/QueryControllerTest.java
+++ src/test/java/org/folio/ldp/QueryControllerTest.java
@@ -2,6 +2,7 @@ package org.folio.ldp;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,7 +12,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMVC
+@AutoConfigureMockMvc
 public class QueryControllerTest {
 
   @Autowired
@@ -19,6 +20,11 @@ public class QueryControllerTest {
 
   @Autowired
   private QueryController queryController;
+
+  @Test
+  public void testTest() {
+
+  }
 
 
 }
