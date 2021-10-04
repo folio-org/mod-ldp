c78659b8a4edc154beffce6d36a2800d6f55d2f2
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2021-10-04 12:12:56 -0400

    Add config entity class, sql class

---
+++ src/main/java/org/folio/ldp/ConfigObj.java
@@ -0,0 +1,31 @@
+package org.folio.ldp;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.folio.ldp.JSONObjectConverter;
+import org.json.JSONObject;
+import org.springframework.lang.NonNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "config")
+public class ConfigObj {
+  @Id
+  private String id;
+
+  @NonNull
+  private String key;
+
+  @NonNull
+  @Column(columnDefinition = "TEXT")
+  @Convert(converter=JSONObjectConverter.class)
+  private JSONObject value;
+}
