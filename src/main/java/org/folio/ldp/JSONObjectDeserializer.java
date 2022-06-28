67b76e1633046f2569dc8323a9f1f3f7e2eacc5a
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2022-06-28 16:46:00 -0400

    Add custom deserializer file

---
+++ src/main/java/org/folio/ldp/JSONObjectDeserializer.java
@@ -0,0 +1,30 @@
+package org.folio.ldp;
+
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class JSONObjectDeserializer extends JsonDeserializer<JSONObject> {
+
+  @Override
+  public JSONObject deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+    JSONObject json;
+    String input = p.readValueAsTree().toString();
+    System.out.println("Attempting to deserialize JSON: " + input);
+    try {
+      Object obj = JSONValue.parseWithException(input);
+      json = (JSONObject) obj;
+    } catch(ParseException pe) {
+      throw new IOException(pe);
+    }
+    return json;
+  }
+  
+}
